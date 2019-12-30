4e39760eb0abc7def29f631e05edec972ba79e3f
chandankumar <33762417+kumar80@users.noreply.github.com>
2019-12-30 20:10:32 +0530

    middleware for user auth using firebase token

--- firebaseAuth.js
+++ firebaseAuth.js
@@ -2,7 +2,7 @@ const {admin,db}  = require('./admin.js');
 
 module.exports = (req,res,next) =>{
     let idToken;
-  
+   
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
       idToken = req.headers.authorization.split('Bearer ')[1];
     }
